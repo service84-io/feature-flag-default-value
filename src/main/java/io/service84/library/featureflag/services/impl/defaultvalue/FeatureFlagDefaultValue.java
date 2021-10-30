1a23fc76d745929cd7589277a79538a04ecd2590
Tyler <Tyler@service84.io>
2021-10-30 18:30:43 +0000

    Use the 1.3.0-SNAPSHOT FeatureFlagAPI

--- src/main/java/io/service84/library/featureflag/services/impl/defaultvalue/FeatureFlagDefaultValue.java
+++ src/main/java/io/service84/library/featureflag/services/impl/defaultvalue/FeatureFlagDefaultValue.java
@@ -2,10 +2,10 @@ package io.service84.library.featureflag.services.impl.defaultvalue;
 
 import org.springframework.stereotype.Service;
 
-import io.service84.library.featureflag.services.FeatureFlagAPI;
+import io.service84.library.featureflag.services.FeatureFlagAPIBase;
 
 @Service("E941C4D7-E3E6-4ED6-B120-0FDFC0C11985")
-public class FeatureFlagDefaultValue implements FeatureFlagAPI {
+public class FeatureFlagDefaultValue extends FeatureFlagAPIBase {
   @Override
   public Boolean getFlag(String flagName, String user, Boolean defaultValue) {
     return defaultValue;
