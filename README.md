831bc123b1ee0cd243cb9a01ba5decf75a056438
Tyler <Tyler@service84.io>
2022-03-29 23:29:25 +0000

    Add Logging

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ This library has a compile-time and run-time dependency on the following librari
 versions are (built-tested)
 
     org.springframework:spring-context:(5.0.0.RELEASE-5.3.3)
+    org.slf4j:slf4j-api:(1.3.0-1.7.30)
 
 ## Build
 This is a Java 11 project that builds best with Gradle 6.3
--- src/main/java/io/service84/library/featureflag/services/impl/defaultvalue/FeatureFlagDefaultValue.java
+++ src/main/java/io/service84/library/featureflag/services/impl/defaultvalue/FeatureFlagDefaultValue.java
@@ -1,13 +1,18 @@
 package io.service84.library.featureflag.services.impl.defaultvalue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import io.service84.library.featureflag.services.FeatureFlagAPIBase;
 
 @Service("E941C4D7-E3E6-4ED6-B120-0FDFC0C11985")
 public class FeatureFlagDefaultValue extends FeatureFlagAPIBase {
+  private static final Logger logger = LoggerFactory.getLogger(FeatureFlagDefaultValue.class);
+
   @Override
   public Boolean getFlag(String flagName, String user, Boolean defaultValue) {
+    logger.debug("getFlag");
     return defaultValue;
   }
 }
